56f92160a30c1291cb6241e7d7194142febe37b3
Слепцов Денис <d.sleptsov@comagic.dev>
2025-07-11 23:36:01 +0300

    Init

---
+++ app/app.py
@@ -0,0 +1,35 @@
+import logging
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('connect')
+def handle_connect():
+    logger.debug('Client connected')
+
+
+@socketio.on('disconnect')
+def handle_disconnect(reason):
+    logger.debug(f'Client disconnected: {reason}')
+
+
+@socketio.on('message')
+def handle_message(data):
+    logger.info(f'Received message from {data}')
+    emit('message', f'Echo: {data}')
+
+
+if __name__ == '__main__':
+    # socketio.run(app, host='localhost', port=8080, debug=True)
+    socketio.run(app, host='localhost', port=8080)
---
+++ app/static/app.js
@@ -0,0 +1,17 @@
+const socket = io("ws://localhost:8080");
+
+socket.on("message", (text) => {
+  const el = document.createElement("li");
+  el.innerHTML = text;
+  document.querySelector("ul").appendChild(el);
+});
+
+document.getElementById("send").onclick = () => {
+  const text_input = document.querySelector("input");
+  try {
+    socket.emit("message", text_input.value);
+  } catch (err) {
+    console.error(err);
+  }
+  text_input.value = "";
+};
