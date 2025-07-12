d8b026dbc164de3e5282fb1b6b1d601042824d1d
Слепцов Денис <d.sleptsov@comagic.dev>
2025-07-12 21:39:11 +0300

    Свистоперделки

--- sales_trainer/static/app.js
+++ sales_trainer/static/app.js
@@ -1,14 +1,22 @@
 const socket = io("ws://localhost:8080");
 //const socket = io("ws://sellmethispen.ru:80");
 
+function add_message_to_dialog(text) {
+  const el = document.createElement("li");
+  el.innerHTML = text;
+  document.querySelector("ul").appendChild(el);
+}
+
 socket.on("message", (text) => {
   const el = document.createElement("li");
   el.innerHTML = text;
   document.querySelector("ul").appendChild(el);
 });
 
-document.getElementById("send").onclick = () => {
-  const text_input = document.querySelector("input");
+send_btn = document.getElementById("send");
+send_btn.onclick = () => {
+  const text_input = document.querySelector("textarea");
+  add_message_to_dialog(text_input.value)
   try {
     socket.emit("message", text_input.value);
   } catch (err) {
@@ -16,3 +24,10 @@ document.getElementById("send").onclick = () => {
   }
   text_input.value = "";
 };
+
+send_btn.addEventListener("keyup", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("send").click();
+  }
+});
